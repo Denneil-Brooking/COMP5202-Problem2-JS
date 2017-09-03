165ed4a18a44f439bba3b67683cc2a665f4674e0
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2017-09-04 11:07:07 +1200

    JS t2 done

---
+++ scripts.js
@@ -0,0 +1,20 @@
+//Programs function
+alert("This Program will display division table 12 times for the number you enter");
+//vars homes
+//ask user for a number
+//retrive user input
+let num = parseInt = prompt("Please enter a number that is below 100", 00);
+//
+if (num < 100)
+    {
+        for (let table = 0; table <= 12; table++)
+        console.log(num + "/" + table + " = " + num/table);
+
+    }
+    else 
+        {
+            console.log("The number you chose " + num + " is over 100");
+            alert("The number you chose " + num + " is over 100");
+        }
+
+
